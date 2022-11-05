bb807ded5b013dfa0e1cd82fef2a6374d384559e
Xianzhao Duan <duanxianzhao@gmail.com>
2022-11-05 14:39:56 -0400

    Update UI

--- app.js
+++ app.js
@@ -8,13 +8,19 @@ async function update() {
         data => {
             console.log(data);
             var list = data[0].assets;
+            // var publish_time = new Date(data[0].published_at).toString();
+
+            // document.getElementById("update-time").innerText = publish_time;
             for (var i = 0; i < list.length - 1; i++) {
                 var ul = document.getElementById("list");
                 var li = document.createElement("li");
+                var icon = document.createElement("div");
+                icon.className = "icon";
                 var aLabel = document.createElement('a');
                 aLabel.innerText = `${list[i].name}`;
                 aLabel.target = "_blank";
                 aLabel.href = `./pdf-viewer/web/viewer.html?file=${list[i].name}`;
+                li.appendChild(icon);
                 li.appendChild(aLabel)
                 ul.appendChild(li);
              }
